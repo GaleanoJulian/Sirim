8815038577b8c581b7aa4cd8f83c5bd5b4c7504b
Danardila <danardila08@gmail.com>
2023-06-19 12:08:54 -0500

    Arreglo de login

--- login.php
+++ login.php
@@ -6,36 +6,49 @@
     <meta name="viewport" content="width=device-width, user-scalable=no initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/estyle2.css">    <title>Iniciar Sesion</title>
+    <script src="./js/regist_validar_campos.js"></script> <!-- Script para poner un spam bajo los campos, indicando que son obligatorios -->
+    <script src="./js/espacioblanco.js"></script>
 </head>
 <body >
-    <div class="container">
-    <section class="zona1">
-        <header>
-                <a href="index.php" class="logo">SIRIM</a>
-            <nav>
-                <ul>
-                    <li><a href="login.php">Iniciar sesión</a></li>
-                    <li><a href="registro.php">Resgistrate</a></li>
-            </nav>
-        </header>
-        <section>
-            <script type="text/javascript">
-                window.addEventListener("scroll", function(){
-                    var header = document.querySelector("header");
-                    header.classList.toggle("abajo",window.scrollY>0);
-                })
-            </script>
 
-        <form action="">
+    <header class="fixed-nav">
+            <a href="index.php" class="logo">SIRIM</a>
+        <nav>
+            <ul>
+                <li><a href="registro.php">Resgistrate</a></li>
+        </nav>
+    </header>
+
+    <main>
+    <form method="post" class="inicio-form">
+            
         <h1>Iniciar Sesión</h1>
-        <input type="email" placeholder="Ingrese su Correo" maxlength="45">
-        <input type="password" placeholder="Ingrese su contraseña" maxlength="10" minlength="5">
-        <button href="./inicio-sesion/sesion.php">Iniciar Sesion</button> <!--Para corregir: Por ahora no funcionará tal cual, solo mientras configuro para validación de información-->
+
+            <div class="input-box">
+                <label>Correo electrónico</label>
+                <input type="email" name="email" placeholder="Ingrese su Correo" maxlength="45" required/>
+                <span class="error-message">Este campo es obligatorio</span>
+            </div>
+
+            <div class="input-box">
+                <label>Contraseña</label>
+                <input type="password" name="password" id="password" placeholder="Ingrese su contraseña" maxlength="50" required onkeyup="validarContraseña()" />
+                <span class="error-message">Este campo es obligatorio</span>
+            </div>
+       
+        
+        <button type="submit" name="login" onclick="validarFormulario()">Iniciar sesión</button>
+
         <p class="link"><a href="registro.php" style="text-decoration: none;">¿No tienes cuenta?</a></p>
-        <p class="link"><a href="restablecer.php" style="text-decoration: none;">¿Olvido su contraseña?</a></p>
-        </form>
-</div>
-<script src="./js/espacioblanco.js"></script>
+        
+        <p class="link"><a href="restablecer.php" style="text-decoration: none;">¿Olvidaste tu contraseña?</a></p>
+        
+    </form>
+    </main>
+
+    <?php
+        include("./conexion-y-logica/conexion_login.php");
+    ?>
 
 </body>
 </html>
