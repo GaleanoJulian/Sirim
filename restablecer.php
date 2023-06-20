a66ce94811a61c8fc6f7db122efbc39033a56694
Danardila <danardila08@gmail.com>
2023-06-20 15:24:01 -0500

    Arreglo en estilo de restablecer

--- restablecer.php
+++ restablecer.php
@@ -7,11 +7,11 @@
     <title>Restablecer contraseña</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/estyle2.css">
+    <script src="./js/espacioblanco.js"></script>
 </head>
 </body>
-<div class="container">
-<section class="zona1">
-    <header>
+
+    <header class="fixed-nav">
             <a href="index.php" class="logo">SIRIM</a>
         <nav>
             <ul>
@@ -19,22 +19,17 @@
                 <li><a href="registro.php">Resgistrate</a></li>
         </nav>
     </header>
-    <section>
-        <script type="text/javascript">
-            window.addEventListener("scroll", function(){
-                var header = document.querySelector("header");
-                header.classList.toggle("abajo",window.scrollY>0);
-            })
-        </script>
-<form method="post">
+
+    <main class="restablecer">
+    <form class="form-restablecer" method="post">
     <h2>¿Olvidaste tu contraseña?</h2><br>
     <p>Ingresa tu correo electrónico para recibir un enlace de restablecimiento de contraseña.</p> <br>
         <div class="input-wrapper">
             <input type="email" placeholder="Ingrese su Correo"  maxlength="45">
         </div>
         <input class="btn" type="submit" name="submit" value="Enviar correo de restablecimiento">
-</form>
-</div>
-<script src="./js/espacioblanco.js"></script>
+    </form>
+    </main>
+
 </body>
 </html>
